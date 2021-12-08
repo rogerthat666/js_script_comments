485233533ecaf28d51ecccfad4240130f32c6c32
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-08 14:35:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy students")
+
+// let multiplication = 1230941 * 1823794;
+// console.log("multiplication:", multiplication);
+
+// let division = 637263 / 54;
+// console.log("division:", division);
+
+/*
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:", division);
+*/
